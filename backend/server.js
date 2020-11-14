b647eeeff28bf5d8e8df3543f06ce3d1d1a88d89
Vadim <vadimoleynik039@gmail.com>
2020-11-14 14:09:59 +0200

    create react app

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const config = require('config')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(bodyParser.json())
+
+app.use('/api', require('./routes/weather.routes.js'))
+
+app.use('*', (req, res) => res.send('Page Not Found'))
+
+app.use('*', (err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+})
+
+const PORT = config.get('port') || 3000
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
